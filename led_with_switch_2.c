52b9fb509d7e3b4286b454db41cc5952cc8a3896
jannis357 <jan.pac@sbg.at>
2019-12-17 19:09:31 +0100

    interrupt routine is running in switch_led_2
    but at the moment it still behaves equally to switch_led_1

--- led_with_switch_2.c
+++ led_with_switch_2.c
@@ -9,24 +9,60 @@
 #define F_CPU 16000000L // Specify oscillator frequency; Needed for _delay_ms()
 #include <avr/io.h>		// The two header files come with the drivers for the Arduino
 #include <util/delay.h>	// They include the _delay_ms() function and some defines (Right click --> Go to implementation)
+#include <avr/sleep.h>
+#include <avr/interrupt.h>
 
-int main(void)
+
+static void led_init(void)
 {
 	DDRB = (1 << 2); // Configures Pin 2 of PORTB as output (should be Pin 10 on arduino); Code: 1 gets shifted by 2
+}
+
+static void button_init(void)
+{
 	DDRB = ~(1 << 4); // Configures Pin 4 of PORTB as input (should be Pin 12 on arduino; must be connected to GND via a switch)
 	
 	PINB |= (1 << 4);	// Pull input Pin at 5V as default
 	
+	PCICR |= (1 << PCIE0);	// enables Pin Change 0 interrupt
+	PCMSK0 |= (1 << PCINT4);	// makes Pin 4 to interrupt pin
+}
+
+static void led_on(void)
+{
+	PORTB |= (1 << 2);
+}
+
+
+static void led_off(void)
+{
+	PORTB &= ~(1 << 2);
+}
+
+ISR(PCINT0_vect) /* pin change interrupt service routine */
+{
+	if (PINB & (1 << 4))	// If input pin is at 5V (default state) --> Output is OFF
+	{
+		led_off();
+	}
+	
+	if (!(PINB & (1 << 4)))	// If input pin is pulled down to 0V (via the physical switch) --> Output is ON
+	{
+		led_on();
+	}
+}
+
+
+
+
+int main(void)
+{
+	led_init();
+	button_init();
+	sei();	// enables interrupts globally
+	
 	while (1)
 	{
-		if (PINB & (1 << 4))	// If input pin is at 5V (default state) --> Output is OFF
-		{
-			PORTB &= ~(1 << 2);
-		}
-		
-		if (!(PINB & (1 << 4)))	// If input pin is pulled down to 0V (via the physical switch) --> Output is ON
-		{
-			PORTB |= (1 << 2);
-		}
+		sleep_mode();
 	}
 }
